4e80270d2d45dea85815509b432b35b1e8981c39
Abbas-Ghazavi <118556497+Abbas-Ghazavi@users.noreply.github.com>
2024-03-25 01:17:23 +0330

    new method square

--- gg.py
+++ gg.py
@@ -229,10 +229,8 @@ class SnakeGameClass:
         return imgMain
 
     def drawSquare(self, imgMain, position, color, fill=False):
-        if fill == True:
-            thickness = 2
-        else:
-            thickness = -2
+        
+        n = self.gameSize[0] // self.numTile
         if (self.high_score <= self.score):
             self.high_score = self.score
         i, j = position
